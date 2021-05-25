c5ae51357fe2789cd252bc0a8652ef636ff820db
reddyrishika <84739444+reddyrishika@users.noreply.github.com>
2021-05-25 16:01:30 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*******************************************************
+* Hello.java
+********************************************************
+* Rishika Reddy
+* 05/25/21
+* CMSC 255 C01
+********************************************************/
+
+public class Hello {
+	public static void main (String [] args) {
+		System.out.println ("<Rishika Reddy>");
+		System.out.println ("<Hello>");
+		System.out.println ("<Lab 1>");
+		System.out.println ("<05/25/21>");
+		System.out.println ("<CMSC 255 C01>");
+		System.out.println ();
+		}
+}
